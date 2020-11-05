eba12f3609512ae8f459f5069c9a1562599dad8b
oloomoses <olooine@gmail.com>
2020-11-05 20:33:57 +0300

    add update record function

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+var selectedRow = null;
+
 function onFormSubmit(){
   let formData = readFormData();
-  insertNewRecord(formData);
+  if(selectedRow == null){
+    insertNewRecord(formData);
+  }else{
+    updateRecord(formData)
+  }
+  
+  formReset();
 }
 
 function readFormData(){
@@ -30,7 +38,29 @@ function insertNewRecord(data){
   cell4.innerHTML = data.city;
 
   cell5 = newRow.insertCell(4);
-  cell5.innerHTML = `<a>Edit</a>
-                      <a>Delete</a>`;
+  cell5.innerHTML = `<a onClick = "onEdit(this)">Edit</a>
+                    <a>Delete</a>`;
 
+}
+
+function formReset(){
+  document.getElementById('fullName').value = '';
+  document.getElementById('empCode').value = '';
+  document.getElementById('salary').value = '';
+  document.getElementById('city').value = '';
+}
+
+function onEdit(td){
+  selectedRow = td.parentElement.parentElement;
+  document.querySelector('#fullName').value = selectedRow.cells[0].innerHTML;
+  document.querySelector('#empCode').value = selectedRow.cells[1].innerHTML;
+  document.querySelector('#salary').value = selectedRow.cells[2].innerHTML;
+  document.querySelector('#city').value = selectedRow.cells[3].innerHTML;
+}
+
+function updateRecord(formData){
+  selectedRow.cells[0].innerHTML = formData.fullName;
+  selectedRow.cells[1].innerHTML = formData.empCode;
+  selectedRow.cells[2].innerHTML = formData.salary;
+  selectedRow.cells[3].innerHTML = formData.city;
 }
